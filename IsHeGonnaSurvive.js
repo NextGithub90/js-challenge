d761799003cd69d4400651e38ba895bd9a2e7746
NextGithub90 <akunbeta958@gmail.com>
2024-03-02 13:27:13 +0700

    tambah soal lagi

---
+++ IsHeGonnaSurvive.js
@@ -0,0 +1,14 @@
+// function hero(bullets, dragons) {
+//   // if (bullets / 2 >= dragons) {
+//   //  return true
+//   // } else {
+//   //     return false
+//   // }
+//   return bullets / 2 > dragons ? true : false;
+// }
+// console.log(hero(10, 5));
+
+function hero(bullets, dragons) {
+  return bullets >= dragons * 2;
+}
+console.log(hero(6, 3));
