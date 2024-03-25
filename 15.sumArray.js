af42a6314d5143268b394e461935155638dba945
NextGithub90 <akunbeta958@gmail.com>
2024-03-25 13:49:16 +0700

    15/366

---
+++ 15.sumArray.js
@@ -0,0 +1,19 @@
+// function sumArray(array) {
+//   if (array == null || array < 2) return 0;
+//   let singkat = array.sort((a, b) => a - b);
+//   let result = 0;
+//   for (let i = 1; i < singkat.length - 1; i++) {
+//     result += singkat[i];
+//   }
+//   return result;
+// }
+// console.log(sumArray([6, 2, 1, 8, 10]));
+
+function sumArray(array) {
+  if (array == null || array.length <= 2) return 0;
+  return array
+    .sort((a, b) => a - b)
+    .slice(1, -1)
+    .reduce((acc, curr) => acc + curr);
+}
+console.log(sumArray([6, 2, 1, 8, 10]));
