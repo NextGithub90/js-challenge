c7a287d7e39cf904bd333a26a2dd7b054270e2de
NextGithub90 <akunbeta958@gmail.com>
2024-03-26 17:16:19 +0700

    16/366

---
+++ 16.meanOfAnArray.js
@@ -0,0 +1,12 @@
+// function getAverage(marks) {
+//   let result = 0;
+//   marks.forEach(function (e, i) {
+//     result += e;
+//   });
+//   return Math.floor(result / marks.length);
+// }
+
+// function getAverage(marks) {
+//   return Math.floor(marks.reduce((acc, curr) => acc + curr) / marks.length);
+// }
+// console.log(getAverage([1, 2, 3, 4, 5]), 3);
